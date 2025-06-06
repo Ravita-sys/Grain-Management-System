9f7eb984e55ac3c18a31adc7d7474560b49255d6
Ravita Prajapati <ravitaprajapati435@gmail.com>
2025-06-06 12:04:20 +0530

    Add files via upload

---
+++ grain/src/main/java/com/example/grain/GrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.grain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrainApplication.class, args);
+	}
+
+}
